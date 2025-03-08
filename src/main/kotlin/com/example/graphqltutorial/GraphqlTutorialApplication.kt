0db5cf0b86c0f7020e36c9da76b202d4159e3b64
Michael <michael.cullen456@gmail.com>
2025-03-08 19:30:11 +0000

    Spring initialiser project creation

---
+++ src/main/kotlin/com/example/graphqltutorial/GraphqlTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphqltutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/graphqltutorial/GraphqlTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.graphqltutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
