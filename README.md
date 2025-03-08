0feaf83e65468cffb14f39694f99a49eac0126fd
Michael <michael.cullen456@gmail.com>
2025-03-08 20:05:31 +0000

    Added subscription

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 Reference: https://spring.io/guides/gs/graphql-server
 
 Local testing: http://localhost:8080/graphiql?path=/graphql
+
+
+Query:
+```graphql
+query bookDetails {
+  bookById(id: "book-1") {
+    id
+    name
+    pageCount
+    author {
+      id
+      firstName
+      lastName
+    }
+  }
+}
+```
+
+Subscription:
+```graphql
+
+```
--- src/main/kotlin/com/example/graphqltutorial/controller/BookController.kt
+++ src/main/kotlin/com/example/graphqltutorial/controller/BookController.kt
@@ -2,10 +2,15 @@ package com.example.graphqltutorial.controller
 
 import com.example.graphqltutorial.model.Author
 import com.example.graphqltutorial.model.Book
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping
 import org.springframework.stereotype.Controller
+import kotlin.random.Random
 
 @Controller
 class BookController {
@@ -17,5 +22,18 @@ class BookController {
     @SchemaMapping
     fun author(book: Book): Author {
         return Author.getById(book.authorId)!!
+    }
+
+    @SubscriptionMapping
+    fun bookAdded(): Flow<Book> = flow {
+        while(true){
+            val id = "book-" + (4 + Random.nextInt(1000))
+            val name = "Random Book " + Random.nextInt(100)
+            val pageCount = 100 + Random.nextInt(900)
+            val authorId = "author-" + (1 + Random.nextInt(3))
+            val book = Book(id, name, pageCount, authorId)
+            emit(book)
+            delay(1000)
+        }
     }
 }
